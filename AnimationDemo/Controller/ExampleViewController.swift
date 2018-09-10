c47f8941bef907b1ac07f73161d9830124fd62e9
Ralf Ebert <info@ralfebert.de>
2018-09-10 19:34:41 +0200

    separate class DropToTargetBehaviour

--- AnimationDemo/Controller/ExampleViewController.swift
+++ AnimationDemo/Controller/ExampleViewController.swift
@@ -1,21 +1,36 @@
 import UIKit
 
+class DropToTargetBehaviour {
+
+    var dragView: UIView!
+    var dropAreaView: UIView!
+
+    init(dragView: UIView, dropAreaView: UIView) {
+        self.dragView = dragView
+        self.dropAreaView = dropAreaView
+
+        self.dragView.addGestureRecognizer(UITapGestureRecognizer(target: self, action: #selector(self.handleTap(_:))))
+    }
+
+    @IBAction func handleTap(_: UITapGestureRecognizer) {
+        self.dragView.center = self.dropAreaView.center
+    }
+}
+
 class ExampleViewController: UIViewController {
 
     @IBOutlet var orangeView: UIView!
     @IBOutlet var dropAreaView: UIImageView!
     @IBOutlet var slider: UISlider!
 
+    var behaviour : DropToTargetBehaviour!
+
     override func viewDidLoad() {
         super.viewDidLoad()
 
         self.orangeView.layer.cornerRadius = 10
 
-        self.orangeView.addGestureRecognizer(UITapGestureRecognizer(target: self, action: #selector(self.handleTap(_:))))
-    }
-
-    @IBAction func handleTap(_: UITapGestureRecognizer) {
-        self.orangeView.center = self.dropAreaView.center
+        self.behaviour = DropToTargetBehaviour(dragView: self.orangeView, dropAreaView: self.dropAreaView)
     }
 
     @IBAction func handleSliderChange() {
